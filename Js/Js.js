5853a28623d1ac3ea224a528de5dc80f988df6e7
ehsan <ehsanesfhany@gmail.com>
2025-01-09 22:49:26 +0330

    Site style

---
+++ Js/Js.js
@@ -0,0 +1,5 @@
+function showSection(sectionId) {
+    document.getElementById('section1').style.display = sectionId === 'section1' ? 'block' : 'none';
+    document.getElementById('section2').style.display = sectionId === 'section2' ? 'block' : 'none';
+}
+
